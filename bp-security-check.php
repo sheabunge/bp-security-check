f8d0ba37e0d6dc39980d59fb3f0aa4936a6f45c1
Shea Bunge <info@bungeshea.com>
2015-07-23 21:41:48 +1000

    Implement Composer

--- bp-security-check.php
+++ bp-security-check.php
@@ -13,8 +13,6 @@
  * Domain Path: /languages/
  */
 
-namespace Shea\BP_Security_Check;
-
 /**
  * Adds a maths sum to the BuddyPress registration page that the user
  * must answer correctly before registering
@@ -23,43 +21,14 @@ namespace Shea\BP_Security_Check;
  * @author Shea Bunge (http://bungeshea.com)
  */
 
-/**
- * Should be set to the Plugin Version defined above
- * @var string
- */
-const VERSION = '1.3.2';
+namespace Shea\BP_Security_Check;
 
-/**
- * Enable autoloading of plugin classes in namespace
- * @param $class_name
- */
-function autoload( $class_name ) {
-
-	/* Only autoload classes from this namespace */
-	if ( false === strpos( $class_name, __NAMESPACE__ ) ) {
-		return;
-	}
-
-	/* Remove namespace from class name */
-	$class_file = str_replace( __NAMESPACE__ . '\\', '', $class_name );
-
-	/* Convert class name format to file name format */
-	$class_file = strtolower( $class_file );
-	$class_file = str_replace( '_', '-', $class_file );
-
-	/* Convert sub-namespaces into directories */
-	$class_path = explode( '\\', $class_file );
-	$class_file = array_pop( $class_path );
-	$class_path = implode( '/', $class_path );
-
-	/* Load the class */
-	require_once __DIR__ . '/php/' . $class_path . '/class-' . $class_file . '.php';
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
 }
 
-spl_autoload_register( __NAMESPACE__ . '\autoload' );
-
 /* Initialise the plugin class */
-$plugin = new Plugin( VERSION, __FILE__ );
+$plugin = new Plugin( '1.3.2', __FILE__ );
 add_action( 'plugins_loaded', array( $plugin, 'run' ) );
 
 /* Make class accessible to other plugins */
